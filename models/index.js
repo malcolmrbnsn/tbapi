86553258d5462d4d1ce9452b70acd4f20c9834ac
robthr <calumrobinson9@gmail.com>
2018-07-15 15:47:40 +1000

    Turned mongoose debug off by default

--- models/index.js
+++ models/index.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
+const env = process.env.NODE_ENV || "development"
+
+if (env !== 'production') {
+  mongoose.set('debug', true);
+}
 // Mongoose
-
-mongoose.set('debug', true);
-
 mongoose.Promise = global.Promise;
 const databaseUri = process.env.DB_URI || "mongodb://localhost/tbapi";
 
-mongoose.connect(databaseUri)
-  .then(() => console.log(`Database connected`))
-  .catch(err => console.log(`Database connection error: ${err.message}`));
+mongoose.connect(databaseUri).
+then(() => console.log(`Database connected`)).
+catch(err => console.log(`Database connection error: ${err.message}`));
 
 module.exports.House = require('./house')
 module.exports.User = require('./user')
