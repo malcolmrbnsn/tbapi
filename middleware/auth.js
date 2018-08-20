8f07b3a06b76db4e7cf037dc1be02116176763ed
robthr <calumrobinson9@gmail.com>
2018-08-20 19:31:52 +1000

    fixed prod login problem

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  req.flash("failure", "Please log in first");
+  req.flash("error", "Please log in first");
 
   return res.redirect("/login");
 };
@@ -14,14 +14,11 @@ exports.isAdmin = (req, res, next) => {
     if (req.user.isAdmin === true) {
       return next();
     }
-    req.flash(
-      "failure",
-      "You don't have the correct permissions to access that"
-    );
+    req.flash("error", "You don't have the correct permissions to access that");
 
     return res.redirect("/login");
   }
-  req.flash("failure", "Please log in first");
+  req.flash("error", "Please log in first");
 
   return res.redirect("/login");
 };
