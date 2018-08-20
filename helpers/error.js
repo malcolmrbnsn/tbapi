4f9bb29a8ab331906b41d9446046cb313c81433d
robthr <calumrobinson9@gmail.com>
2018-08-20 19:27:20 +1000

    Added console logging (Diagnosing prod problem)

--- helpers/error.js
+++ helpers/error.js
@@ -1,6 +1,7 @@
 // eslint-disable-line
 function errorHandler(error, req, res, next) {
   req.flash("error", error.message);
+  console.log(error);
 
   if (req.isAuthenticated()) {
     return res.redirect("/houses");
